498f70efe2b31b01ab14df38f2c1c20106756b9a
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2024-10-29 14:28:40 +0800

    店铺

--- database/migrations/2020_04_09_000000_create_subscriptions_table.php
+++ database/migrations/2020_04_09_000000_create_subscriptions_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+//订阅博客、商店、之类的
 class CreateSubscriptionsTable extends Migration
 {
     /**
--- database/migrations/2024_05_13_000001_create_email_subscribers_table.php
+++ database/migrations/2024_05_13_000001_create_email_subscribers_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+//订阅主战
 return new class extends Migration {
     protected $table = 'blw_email_subscribers';
 
